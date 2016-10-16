d1bf7749491c58c07a3b03d6b0cca922c2fee745
Arian <ajahiri.AIT@gmail.com>
2016-10-17 00:18:16 +1100

    Build Final Version
    Finished the program, final version should be 1.0.0.13.

--- project/pingChecker_LOL/Properties/AssemblyInfo.cs
+++ project/pingChecker_LOL/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("LoL Ping Checker")]
 [assembly: AssemblyDescription("Ping checking tool for League of Legends.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("AIT-Archive")]
+[assembly: AssemblyCompany("Arian.TECH")]
 [assembly: AssemblyProduct("pingChecker_LOL")]
 [assembly: AssemblyCopyright("Copyright Arian Jahiri ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.13")]
+[assembly: AssemblyFileVersion("1.0.0.13")]
